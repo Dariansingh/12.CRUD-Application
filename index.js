bc1f8d57e24c1dd98564feb4ab6e6f246dc893af
Darian Singh <darian.singh@gmail.com>
2023-01-20 16:59:07 -0600

    Third Commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+document.getElementById("create-new-movie").addEventListener("click", function(event) {
+    event.preventDefault();
+    createNewMovie();
+});
+
+document.getElementById("app").addEventListener("click", function(event) {
+    if(event.target.classList.contains("delete")) {
+      let id = event.target.getAttribute("data-id");
+      deleteMovie(id);
+    }
+});
+
 class Movie {
     constructor(Name, Rating, Genre) {
         this.Name = Name
@@ -5,6 +17,27 @@ class Movie {
         this.Genre = Genre
     }
 }
+
+function createNewMovie() {
+    let name = document.getElementById("Name").value;
+    let genre = document.getElementById("Genre").value;
+    let rating = document.getElementById("Rating").value;
+
+    let newMovie = new Movie(name, rating, genre);
+    DOMManager.createMovieData(newMovie);
+}
+
+function deleteMovie(id) {
+    $.ajax({
+        url: `${MovieService.url}/${id}`,
+        type: "DELETE"
+    })
+    .then(() => {
+        return MovieService.getAllMovies();
+    })
+    .then((data) => DOMManager.render(data));
+}
+
 
 class MovieService {
     static url = "https://63c87820075b3f3a91e1846e.mockapi.io/movies"
@@ -18,14 +51,42 @@ class MovieService {
 
 /*class that lets the movie data be seen */
 class DOMManager {
+
     static getAllProducts() {
-        MovieService.getAllMovies().then((data) => this.movieData(data))
+        MovieService.getAllMovies().then((data) => this.render(data))
     }
 
-    static movieData(apiData) {
-        console.log('movie', apiData[0].Name)
+    static createMovieData(movie) {
+        $.ajax({
+          url: MovieService.url,
+          type: "POST",
+          data: movie
+        })
+        .then(() => {
+          return MovieService.getAllMovies();
+        })
+        .then((data) => this.render(data));
     }
+    
+
+    static render(data) {
+        let app = document.getElementById("app");
+        let html = "";
+        data.forEach((movie) => {
+            html += `<div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${movie.Name}</h5>
+                            <p class="card-text">Rating: ${movie.Rating}</p>
+                            <p class="card-text">Genre: ${movie.Genre}</p>
+                            <button class="btn btn-danger delete" data-id="${movie.id}">Delete</button>
+                        </div>
+                    </div>`;
+        });
+        app.innerHTML = html;
+    }
+    
 }
+
 
 DOMManager.getAllProducts()
 
