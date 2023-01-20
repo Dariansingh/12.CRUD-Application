948a807ff10aff2676580d386e7ab9b203dfe63e
Darian Singh <darian.singh@gmail.com>
2023-01-20 15:22:27 -0600

    second commit

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ class Movie {
 }
 
 class MovieService {
-    static url = 'https://63c87820075b3f3a91e1846e.mockapi.io/movies'
+    static url = "https://63c87820075b3f3a91e1846e.mockapi.io/movies"
 
     static getAllMovies() {
         return $.get(this.url);
     }
 }
+
+
+
+/*class that lets the movie data be seen */
+class DOMManager {
+    static getAllProducts() {
+        MovieService.getAllMovies().then((data) => this.movieData(data))
+    }
+
+    static movieData(apiData) {
+        console.log('movie', apiData[0].Name)
+    }
+}
+
+DOMManager.getAllProducts()
 
